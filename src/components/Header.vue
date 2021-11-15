8bfe7d88ace1a7c4afbaed61a234e69277fc9f51
iamfrego <mate_freg@tiscali.it>
2021-11-15 12:19:22 +0100

    Header Complete

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="header"></div>
+  <div class="header">
+    <img src="../assets/image/Spotify-Logo.svg" alt="Logo" />
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+
+.header {
+  background-color: $secondary;
+  img {
+    padding: 1rem 2rem;
+    height: 75px;
+  }
+}
 </style>
