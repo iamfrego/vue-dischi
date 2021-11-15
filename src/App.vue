85caf23be4b9c1cdd57f16890e46bfbb9a249368
iamfrego <mate_freg@tiscali.it>
2021-11-15 12:04:55 +0100

    Components create

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+    Main,
+  },
 };
 </script>
 
